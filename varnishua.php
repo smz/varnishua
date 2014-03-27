de3a68cfce8de11a1618fe55ce873f474c0fcb69
dimitris <dimitris@dimitriss-MacBook-Pro.local>
2014-03-27 21:06:12 +0200

    updated
    latest php mobile detect lib, change in storing the state
    $app->setUserState('varnishua.device', $layout);
    use
    $app->getUserState('varnishua.device', $layout);
    
    Signed-off-by: dimitris <dimitris@dimitriss-MacBook-Pro.local>

--- varnishua.php
+++ varnishua.php
@@ -1,68 +1,38 @@
 <?php
-/**
- * ---------------------------------------------------------------------------------------------------------
- * 	Varnish User Agent plugin
- *
- * Copyright (C) 2013 Dimitris Grammatikogiannis. All rights reserved.
- *
- * Varnish User Agent is free software and is distributed under the GNU General Public License,
- * and as distributed it may include or be derivative of works licensed under the GNU
- * General Public License or other free or open source software licenses.
- *	pc
- *	bot
- *	tablet-ipad
- *	mobile-iphone
- *	mobile-android
- *	tablet-android
- *	mobile-firefoxos
- *	mobile-smartphone
- *	mobile-generic
- * --------------------------------------------------------------------------------------------------------- 
- **/
-// no direct access
-
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 jimport( 'joomla.plugin.plugin' );
 
-/**
- * VARNISH, PHP UA Plugin
- */
 class plgSystemVarnishua extends JPlugin
 {
-			
-	public function onAfterInitialise()
+	function onAfterInitialise()
 	{
-		if (!$this->params->get('varnish_setup')) {
-//		echo "php staff ok";			
-		include_once(dirname(__FILE__).'/lib/Mobile_Detect.php');
-		$detect = new Mobile_Detect();
-		$layout = ($detect->isMobile() ? ($detect->isTablet() ? 'tablet' : 'mobile') : 'desktop');
-		if (($detect->is('Bot')) || ($detect->is('MobileBot'))) $layout = 'bot';
-		$session = JFactory::getSession();
-		$session->set('ualayout', $layout);
+		$app = JFactory::getApplication();
+
+		if ( $app->isAdmin() ) {
+			return;
 		}
-		
-		else {
-//		echo "varnish staff ok";
-		$badbrowser = $this->params->get('redirect_link');				
-		$user = JFactory::getUser();
-		if (!$user->guest) {
-			JResponse::setHeader('X-Logged-In', 'True', true);
-			} else {
-			JResponse::setHeader('X-Logged-In', 'False', true);	
-		}			
-		$layout = $_SERVER['HTTP_X_UA_DEVICE'];
-		$session = JFactory::getSession();
-	
-		if ($layout == 'pc') { $session->set('ualayout', 'desktop'); }
-		if ($layout == 'bot') { $session->set('ualayout', 'bot'); }
-		if (($layout == 'mobile-iphone') || ($layout == 'mobile-android') || ($layout == 'mobile-firefoxos') || ($layout == 'mobile-smartphone') || ($layout == 'mobile-generic')) { $session->set('ualayout', 'mobile'); }
-		if (($layout == 'tablet-ipad') || ($layout == 'tablet-android')) { $session->set('ualayout', 'tablet'); }
-		if ($layout == 'bad') { 
-			header('Location: '.$badbrowser);
-					die();
-				}			
+
+		if ($this->params->get('varnish_setup', 0) == "0") { 
+			//Using PHP;
+			include_once(dirname(__FILE__).'/lib/Mobile_Detect.php');
+			$detect = new Mobile_Detect();
+			$layout = ($detect->isMobile() ? ($detect->isTablet() ? 'tablet' : 'mobile') : 'desktop');
+			if (($detect->is('Bot')) || ($detect->is('MobileBot'))) $layout = 'bot';
+				$app->setUserState('varnishua.device', $layout);
+		} else { 
+			//Using Varnish
+			$badbrowser = $this->params->get('redirect_link');
+			if (isset($_SERVER['HTTP_X_UA_DEVICE'])) {
+				$layout = $_SERVER['HTTP_X_UA_DEVICE'];
+			} else { $layout = 'pc'; }
+			if ($layout == 'pc') { $app->setUserState('varnishua.device', 'desktop'); }
+			if ($layout == 'bot') { $app->setUserState('varnishua.device', 'bot'); }
+			if (($layout == 'mobile-iphone') || ($layout == 'mobile-android') || ($layout == 'mobile-firefoxos') || ($layout == 'mobile-smartphone') || ($layout == 'mobile-generic')) { $app->setUserState('varnishua.device', 'mobile'); }
+			if (($layout == 'tablet-ipad') || ($layout == 'tablet-android')) { $app->setUserState('varnishua.device', 'tablet'); }
+			if ($layout == 'bad') {
+				header('Location: '.$badbrowser);
+				die(); }
 		}
 	}
 }
